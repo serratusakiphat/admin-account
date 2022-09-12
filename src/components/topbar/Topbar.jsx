a1ba47a8b71b646716e1b5cfc80a96e795dbed26
Scipio Mundine <sm3@Serratusakiphat-mac.local>
2022-09-12 01:31:34 -0700

    admin account

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./topbar.css";
+import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="topbarWrapper">
+        <div className="topLeft">
+          <span className="logo">ScipioAdmin</span>
+        </div>
+        <div className="topRight">
+          <div className="topbarIconContainer">
+            <NotificationsNone />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Language />
+            <span className="topIconBadge">2</span>
+          </div>
+          <div className="topbarIconContainer">
+            <Settings />
+          </div>
+          <img
+            src="../updateHeadshot-PhotoRoom.png"
+            alt=""
+            className="topAvatar"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
